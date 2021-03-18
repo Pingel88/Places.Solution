45d16a48f60d6422cc5aaf361654a4026c2c6866
Michael Pingel <mdpingel@gmail.com>
2021-03-18 15:22:04 -0700

    add views for cities, people, and home
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

---
+++ Places/Migrations/20210318204405_initial.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Places.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Landmarks",
+                columns: table => new
+                {
+                    LandmarkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Landmarks", x => x.LandmarkId);
+                    table.ForeignKey(
+                        name: "FK_Landmarks_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CityPerson",
+                columns: table => new
+                {
+                    CityPersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CityId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CityPerson", x => x.CityPersonId);
+                    table.ForeignKey(
+                        name: "FK_CityPerson_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CityPerson_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LandmarkPerson",
+                columns: table => new
+                {
+                    LandmarkPersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LandmarkId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LandmarkPerson", x => x.LandmarkPersonId);
+                    table.ForeignKey(
+                        name: "FK_LandmarkPerson_Landmarks_LandmarkId",
+                        column: x => x.LandmarkId,
+                        principalTable: "Landmarks",
+                        principalColumn: "LandmarkId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LandmarkPerson_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CityPerson_CityId",
+                table: "CityPerson",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CityPerson_PersonId",
+                table: "CityPerson",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LandmarkPerson_LandmarkId",
+                table: "LandmarkPerson",
+                column: "LandmarkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LandmarkPerson_PersonId",
+                table: "LandmarkPerson",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Landmarks_CityId",
+                table: "Landmarks",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CityPerson");
+
+            migrationBuilder.DropTable(
+                name: "LandmarkPerson");
+
+            migrationBuilder.DropTable(
+                name: "Landmarks");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
